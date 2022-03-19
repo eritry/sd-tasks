561505f64bbe2b188c2fff0953e5be2b6c398c45
eritry <erika@DESKTOP-QP06AAM.localdomain>
2022-03-19 11:42:56 +0300

    hw5 init commit

---
+++ graphs/src/main/java/drawing/Point.java
@@ -0,0 +1,34 @@
+package drawing;
+
+public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof Point)) return false;
+        Point point = (Point) other;
+        double EPS = 1e-10;
+        return Math.abs(x - point.x) < EPS &&
+                Math.abs(y - point.y) < EPS;
+    }
+
+    @Override
+    public String toString() {
+        return "Point { x: " + x + ", y: " + y + " }";
+    }
+}
---
+++ graphs/src/main/java/graph/Graph.java
@@ -0,0 +1,86 @@
+package graph;
+
+import drawing.DrawingApi;
+import drawing.Point;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static drawing.DrawingUtils.MARGIN;
+import static drawing.DrawingUtils.VERTEX_RADIUS;
+
+public abstract class Graph {
+    private final DrawingApi drawingApi;
+    private final Map<Vertex, Point> drawnVertices = new HashMap<>();
+    private final Set<Edge> drawnEdges = new HashSet<>();
+    private PointsFactory pointsFactory;
+
+    public Graph(DrawingApi drawingApi) {
+        this.drawingApi = drawingApi;
+    }
+
+    public void drawGraph() {
+        Point center = new Point(
+                drawingApi.getDrawingAreaWidth() / 2.0,
+                drawingApi.getDrawingAreaHeight() / 2.0
+        );
+        double radius = Math.min(
+                drawingApi.getDrawingAreaWidth(),
+                drawingApi.getDrawingAreaHeight()
+        ) / 2.0 - MARGIN;
+
+        pointsFactory = new PointsFactory(getGraphSize(), center, radius);
+
+        doDrawGraph();
+        drawingApi.visualize();
+    }
+
+    protected Point drawVertex(Vertex vertex) {
+        if (drawnVertices.containsKey(vertex)) {
+            return drawnVertices.get(vertex);
+        }
+
+        Point point = pointsFactory.nextPoint();
+        drawingApi.drawCircle(point, VERTEX_RADIUS);
+        drawnVertices.put(vertex, point);
+        return point;
+    }
+
+    protected void drawEdge(Edge edge) {
+        if (drawnEdges.contains(edge)) {
+            return;
+        }
+
+        Point a = drawVertex(edge.getFrom());
+        Point b = drawVertex(edge.getTo());
+        drawingApi.drawLine(a, b);
+        drawnEdges.add(edge);
+    }
+
+    public void readGraphFromConsole() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            readGraph(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public void readGraphFromFile(String fileName) throws UncheckedIOException {
+        Path file = Paths.get(fileName);
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file.toFile())))) {
+            readGraph(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    protected abstract void doDrawGraph();
+
+    protected abstract void readGraph(BufferedReader reader);
+
+    protected abstract int getGraphSize();
+}
